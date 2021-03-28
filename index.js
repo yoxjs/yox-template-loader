c075b7dc3a9f76c4c8563508f0fb20e06573ae01
tiva <752990270@qq.com>
2021-03-28 20:57:51 +0800

    bump 0.4.0

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Yox = require('yox')
 
+Yox.config.uglifyCompiled = true
+Yox.config.minifyCompiled = true
+
 module.exports = function (content) {
   return `module.exports = ${Yox.compile(content, true)};`
 }
